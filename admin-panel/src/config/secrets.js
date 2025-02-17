4a07ff87cca79592bfab25817454fbf489cd3cbf
frank-and-hub <sourabbiswas000x@gmail.com>
2025-02-17 20:58:28 +0530

    switch to new  dynamic database

--- admin-panel/src/config/secrets.js
+++ admin-panel/src/config/secrets.js
@@ -5,9 +5,9 @@ const processed_env = process.env;
 const connectionFunction = (processed_env) => {
     switch (processed_env.DB_TYPE) {
         case `localhost`:
-            return `mongodb://localhost:27017/${processed_env.DB_NAME}`;
+            return `mongodb://localhost:27017/${processed_env.CONNECTION_NAME}`;
         case `live`:
-            return `${processed_env.DB_URL}://${processed_env.DB_NAME}:${processed_env.DB_PWD}@${processed_env.DB_NAME}.${processed_env.DB_CONNECTION}=${processed_env.DB_NAME}`
+            return `${processed_env.DB_URL}://${processed_env.CONNECTION_NAME}:${processed_env.DB_PWD}@${processed_env.CONNECTION_NAME}.${processed_env.DB_CONNECTION_BEFORE}${processed_env.DB_NAME}${processed_env.DB_CONNECTION_AFTER}=${processed_env.CONNECTION_NAME}`
         default:
             throw new Error('Invalid DB_TYPE specified in the environment variables.');
     }
